5d1a042f9bb17852e1a249c673d6d4f5374a329b
Ravencl4w <106709131+Ravencl4w@users.noreply.github.com>
2022-07-09 11:13:09 -0500

    ultimo commit

--- ms-producto/src/main/java/com/ejemplo/msproducto/controllers/ProductController.java
+++ ms-producto/src/main/java/com/ejemplo/msproducto/controllers/ProductController.java
@@ -1,9 +1,14 @@
 package com.ejemplo.msproducto.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +24,24 @@ public class ProductController {
     @Autowired
     private ProductService pService;
 
+    @GetMapping
     public ResponseEntity<List<Product>> listAll() {
         return ResponseEntity.ok(pService.getAllProducts());
+    }
+    @PostMapping
+    private ResponseEntity<Product> savePlan (@RequestBody Product product) {
+        
+        Product tempProd = pService.create(product);
+
+        try {
+
+            return ResponseEntity.created(new URI("/api/plans"+tempProd.getId())).body(tempProd);
+
+        } catch (Exception e) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+        }   
     }
     
 }
